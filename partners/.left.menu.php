569cdb32cb6059b4e276cddbda3325cd3638220d
Hoqi <root@bitrix.bitrixdomain>
2020-12-25 00:07:14 +0300

    [ex1-1] Add section partners

---
+++ partners/.left.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Условия сотрудничества", 
+		"/partners/index.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Как стать партнером", 
+		"/partners/how-to-partners.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Расписание мероприятий", 
+		"/partners/schedule-event/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
---
+++ partners/schedule-event/.left.menu.php
@@ -0,0 +1,18 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Анонсы", 
+		"/partners/schedule-event/index.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Архив", 
+		"/partners/schedule-event/archive.php", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
