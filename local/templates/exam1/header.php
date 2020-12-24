e58659fd2ca38f6d4421237d20e87b73589597aa
Hoqi <root@bitrix.bitrixdomain>
2020-12-25 01:14:04 +0300

    [ex1-4] add template with diff view formain/inter page

---
+++ local/templates/exam1/header.php
@@ -0,0 +1,173 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+IncludeTemplateLangFile(__FILE__);
+?>
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <?$APPLICATION->ShowHead();?>
+    <title>
+        <?$APPLICATION->ShowTitle()?>
+    </title>
+    <?$APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH.'/css/style.css');?>
+    <?$APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH.'/css/reset.css');?>
+    <?$APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH.'/css/owl.carousel.css');?>
+
+    <?$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH.'/js/jquery.min.js');?>
+    <?$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH.'/js/owl.carousel.min.js');?>
+    <?$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH.'/js/scripts.js');?>
+    <link rel="icon" type="image/vnd.microsoft.icon" href="favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="<?= SITE_TEMPLATE_PATH ?>/img/favicon.ico">
+</head>
+
+<body>
+    <div id="panel">
+        <?$APPLICATION->ShowPanel();?>
+    </div>
+    <!-- wrap -->
+    <div class="wrap">
+        <!-- header -->
+        <header class="header">
+            <div class="inner-wrap">
+                <div class="logo-block"><a href="" class="logo">Мебельный магазин</a>
+                </div>
+                <div class="main-phone-block">
+                    <? if ( date('H') < 18 && date('H') > 9): ?>
+                    <a href="tel:84952128506" class="phone">8 (495) 212-85-06</a>
+                    <? else: ?>
+                    <a href="mailto:store@store.ru" class="phone">store@store.ru</a>
+                    <? endif; ?>
+                    <div class="shedule">время работы с 9-00 до 18-00</div>
+                </div>
+                <div class="actions-block">
+                    <form action="/" class="main-frm-search">
+                        <input type="text" placeholder="Поиск">
+                        <button type="submit"></button>
+                    </form>
+                    <nav class="menu-block">
+                        <ul>
+                            <li class="att popup-wrap">
+                                <a id="hd_singin_but_open" href="" class="btn-toggle">Войти на сайт</a>
+                                <form action="/" class="frm-login popup-block">
+                                    <div class="frm-title">Войти на сайт</div>
+                                    <a href="" class="btn-close">Закрыть</a>
+                                    <div class="frm-row"><input type="text" placeholder="Логин"></div>
+                                    <div class="frm-row"><input type="password" placeholder="Пароль"></div>
+                                    <div class="frm-row"><a href="" class="btn-forgot">Забыли пароль</a></div>
+                                    <div class="frm-row">
+                                        <div class="frm-chk">
+                                            <input type="checkbox" id="login">
+                                            <label for="login">Запомнить меня</label>
+                                        </div>
+                                    </div>
+                                    <div class="frm-row"><input type="submit" value="Войти"></div>
+                                </form>
+                            </li>
+                            <li><a href="">Зарегистрироваться</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+        <!-- /header -->
+        <!-- nav -->
+        <nav class="nav">
+            <div class="inner-wrap">
+                <div class="menu-block popup-wrap">
+                    <a href="" class="btn-menu btn-toggle"></a>
+                    <div class="menu popup-block">
+                        <ul class="">
+                            <li class="main-page"><a href="">Главная</a>
+                            </li>
+                            <li>
+                                <a href="">Компания</a>
+                                <ul>
+                                    <li>
+                                        <a href="">Пункт 1</a>
+                                        <ul>
+                                            <li><a href="">Пункт 1</a>
+                                            </li>
+                                            <li><a href="">Пункт 2</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="">Пункт 2</a>
+                                    </li>
+                                    <li><a href="">Пункт 3</a>
+                                    </li>
+                                    <li><a href="">Пункт 4</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a href="">Новости</a>
+                            </li>
+                            <li>
+                                <a href="">Каталог</a>
+                                <ul>
+                                    <li>
+                                        <a href="">Пункт 1</a>
+                                        <ul>
+                                            <li><a href="">Пункт 1</a>
+                                            </li>
+                                            <li><a href="">Пункт 2</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="">Пункт 2</a>
+                                    </li>
+                                    <li><a href="">Пункт 3</a>
+                                    </li>
+                                    <li><a href="">Пункт 4</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a href="">Фотогалерея</a>
+                            </li>
+                            <li><a href="">Партнерам</a>
+                            </li>
+                            <li><a href="">Контакты</a>
+                            </li>
+                        </ul>
+                        <a href="" class="btn-close"></a>
+                    </div>
+                    <div class="menu-overlay"></div>
+                </div>
+            </div>
+        </nav>
+        <!-- /nav -->
+        <? if ($APPLICATION->GetCurPage() != '/'): ?>
+        <!-- breadcrumbs -->
+        <div class="breadcrumbs-box">
+            <div class="inner-wrap">
+                <a href="">Главная</a>
+                <a href="">Мебель</a>
+                <span>Выставки и события</span>
+            </div>
+        </div>
+        <!-- /breadcrumbs -->
+        <? endif; ?>
+        <!-- page -->
+        <div class="page">
+            <!-- content box -->
+            <div class="content-box">
+                <!-- content -->
+                <div class="content">
+                    <div class="cnt">
+                        <? if ($APPLICATION->GetCurPage() != '/'): ?>
+                        <header>
+                            <h1>
+                                <?$APPLICATION->ShowTitle(true)?>
+                            </h1>
+                        </header>
+                        <hr>
+                        <? else: ?>
+                        <?
+                            $APPLICATION->IncludeFile(
+                                SITE_TEMPLATE_PATH . "/include/mainContent.php",
+                                Array(),
+                                Array("MODE" => "html")
+                                );
+                        ?>
+                        <? endif; ?>
