fbec0a5fc1573ab9e37b31b122c77db7ea54953b
Hoqi <root@bitrix.bitrixdomain>
2020-12-25 12:43:40 +0300

    [ex1-20] add auth form

--- local/templates/exam1/header.php
+++ local/templates/exam1/header.php
@@ -45,53 +45,42 @@ IncludeTemplateLangFile(__FILE__);
                         <input type="text" placeholder="Поиск">
                         <button type="submit"></button>
                     </form>
-                    <nav class="menu-block">
-                        <ul>
-                            <li class="att popup-wrap">
-                                <a id="hd_singin_but_open" href="" class="btn-toggle">Войти на сайт</a>
-                                <form action="/" class="frm-login popup-block">
-                                    <div class="frm-title">Войти на сайт</div>
-                                    <a href="" class="btn-close">Закрыть</a>
-                                    <div class="frm-row"><input type="text" placeholder="Логин"></div>
-                                    <div class="frm-row"><input type="password" placeholder="Пароль"></div>
-                                    <div class="frm-row"><a href="" class="btn-forgot">Забыли пароль</a></div>
-                                    <div class="frm-row">
-                                        <div class="frm-chk">
-                                            <input type="checkbox" id="login">
-                                            <label for="login">Запомнить меня</label>
-                                        </div>
-                                    </div>
-                                    <div class="frm-row"><input type="submit" value="Войти"></div>
-                                </form>
-                            </li>
-                            <li><a href="">Зарегистрироваться</a>
-                            </li>
-                        </ul>
-                    </nav>
+                    <? $APPLICATION->IncludeComponent(
+                        "bitrix:system.auth.form", 
+                        "auth", 
+                        array(
+                            "FORGOT_PASSWORD_URL" => "/login/index.php?forgot_password=yes",
+                            "PROFILE_URL" => "/login/user.php",
+                            "REGISTER_URL" => "/login/index.php?register=yes",
+                            "SHOW_ERRORS" => "Y",
+                            "COMPONENT_TEMPLATE" => "auth"
+                        ),
+                        false
+                    ); ?>
                 </div>
             </div>
         </header>
         <!-- /header -->
         <!-- nav -->
         <?$APPLICATION->IncludeComponent(
-	"bitrix:menu", 
-	"top_multi", 
-	array(
-		"ALLOW_MULTI_SELECT" => "N",
-		"CHILD_MENU_TYPE" => "left",
-		"COMPONENT_TEMPLATE" => "top_multi",
-		"DELAY" => "N",
-		"MAX_LEVEL" => "3",
-		"MENU_CACHE_GET_VARS" => array(
-		),
-		"MENU_CACHE_TIME" => "360000",
-		"MENU_CACHE_TYPE" => "N",
-		"MENU_CACHE_USE_GROUPS" => "Y",
-		"ROOT_MENU_TYPE" => "top",
-		"USE_EXT" => "N"
-	),
-	false
-);?>
+            "bitrix:menu", 
+            "top_multi", 
+            array(
+                "ALLOW_MULTI_SELECT" => "N",
+                "CHILD_MENU_TYPE" => "left",
+                "COMPONENT_TEMPLATE" => "top_multi",
+                "DELAY" => "N",
+                "MAX_LEVEL" => "3",
+                "MENU_CACHE_GET_VARS" => array(
+                ),
+                "MENU_CACHE_TIME" => "360000",
+                "MENU_CACHE_TYPE" => "N",
+                "MENU_CACHE_USE_GROUPS" => "Y",
+                "ROOT_MENU_TYPE" => "top",
+                "USE_EXT" => "N"
+            ),
+            false
+        );?>
         <!-- /nav -->
         <? if ($APPLICATION->GetCurPage() != '/'): ?>
         <!-- breadcrumbs -->
